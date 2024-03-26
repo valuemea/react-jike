466caee08e3a1afe58e630c1e09b96042f3be741
sunxin <sunxin@zqiot.com>
2024-03-26 10:39:54 +0800

    'axios添加请求拦截器，在axios请求头注入tok'

--- src/untils/request.js
+++ src/untils/request.js
@@ -2,6 +2,7 @@
 // 1. 根域名配置 2. 超时时间 3. 请求拦截器 / 响应拦截器
 
 import axios from "axios";
+import { getToken } from "./token";
 
 // 自定义创建一个axios实例
 const request = axios.create({
@@ -12,6 +13,11 @@ const request = axios.create({
 // 添加请求拦截器,可以插入一些自定义的配置，一般是请求参数的处理
 request.interceptors.request.use(function (config) {
   // 在发送请求之前做些什么
+  // 注入token数据  1. 获取token 2. 按照后端的格式要求做 token 拼接
+  const token = getToken()
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}` // 添加请求头 Authorization(token数据)
+  }
   return config;
 }, function (error) {
   // 对请求错误做些什么
